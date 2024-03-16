05a0e31dc4a97661b9f88e05192b3f4070ee9d00
Rajesh <rajesh.d2022ai-ml@sece.ac.in>
2024-03-17 00:03:19 +0530

    login

--- src/App.js
+++ src/App.js
@@ -7,24 +7,25 @@ import Landing from './components/Landing';
 import Saloon from './components/Saloon';
 import Cart from './components/cart';
 import PasswordLoginWithFirebase from './components/PasswordLoginWithFirebase';
+import FoodCourt from './components/Food_court';
 
 
 function App() {
   return (
-    <PasswordLoginWithFirebase/>
-    // <div>
-    // <Router>
-    //   <Routes>
-    //     <Route path="" element={<Landing />} />
-    //     <Route path="/" element={<Landing />} />
-    //     <Route path="/foodcourt" element={<PasswordLoginWithFirebase />} />
-    //     <Route path="/store" element={<Store />} />
-    //     <Route path="/saloon" element={<Saloon />} />
-    //     <Route path="/cart" element={<Cart />} />
+    // <PasswordLoginWithFirebase/>
+    <div>
+    <Router>
+      <Routes>
+        <Route path="" element={<Landing />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/foodcourt" element={<Food/>} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/saloon" element={<Saloon />} />
+        <Route path="/cart" element={<Cart />} />
 
-    //   </Routes>
-    // </Router>
-    // </div>
+      </Routes>
+    </Router>
+    </div>
   );
 }
 
--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,20 +1,20 @@
-// import { initializeApp } from 'firebase/app';
-// import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDFAfvJfoDLTYa2OvKKWXzyZr8UN7G3meE",
-//     authDomain: "webathon-sece.firebaseapp.com",
-//     projectId: "webathon-sece",
-//     storageBucket: "webathon-sece.appspot.com",
-//     messagingSenderId: "81525963697",
-//     appId: "1:81525963697:web:72ca46fe601067d2bddb90",
-//     measurementId: "G-89ME6HWLJM"
-// };
+const firebaseConfig = {
+    apiKey: "AIzaSyDFAfvJfoDLTYa2OvKKWXzyZr8UN7G3meE",
+    authDomain: "webathon-sece.firebaseapp.com",
+    projectId: "webathon-sece",
+    storageBucket: "webathon-sece.appspot.com",
+    messagingSenderId: "81525963697",
+    appId: "1:81525963697:web:72ca46fe601067d2bddb90",
+    measurementId: "G-89ME6HWLJM"
+};
 
 
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-// export { db };
+export { db };
 
