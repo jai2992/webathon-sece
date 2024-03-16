ae25761251f15db171f59bb40bc55667af6d1727
Rajesh <rajesh.d2022ai-ml@sece.ac.in>
2024-03-16 17:04:51 +0530

    nothing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Store from './components/store';
 import Food from './components/Food_court'
 import Landing from './components/Landing';
+import Cart from './components/cart';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/foodcourt" element={<Food />} />
         <Route path="/store" element={<Store />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
     </div>
